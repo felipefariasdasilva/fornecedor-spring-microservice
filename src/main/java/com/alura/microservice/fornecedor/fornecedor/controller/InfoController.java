15d83221be1c1b6b5bd6e7a2db05cbbe7dcc5248
felipefariasdasilva <feliipefarias@outlook.com>
2020-04-27 19:17:08 -0300

    added bootstrap.yml configuration

--- src/main/java/com/alura/microservice/fornecedor/fornecedor/controller/InfoController.java
+++ src/main/java/com/alura/microservice/fornecedor/fornecedor/controller/InfoController.java
@@ -17,6 +17,6 @@ public class InfoController {
 
     @RequestMapping("/{estado}")
     public InfoForncedor getInfoPorEstado(@PathVariable String estado){
-       return infoService.getInfoPorEstado(estado);
+        return infoService.getInfoPorEstado(estado);
     }
 }
--- src/main/java/com/alura/microservice/fornecedor/fornecedor/service/InfoService.java
+++ src/main/java/com/alura/microservice/fornecedor/fornecedor/service/InfoService.java
@@ -3,11 +3,13 @@ package com.alura.microservice.fornecedor.fornecedor.service;
 import com.alura.microservice.fornecedor.fornecedor.model.InfoForncedor;
 import com.alura.microservice.fornecedor.fornecedor.repository.InfoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class InfoService {
 
+    @Autowired
     private InfoRepository InfoRepository;
 
     public InfoForncedor getInfoPorEstado(String estado){
